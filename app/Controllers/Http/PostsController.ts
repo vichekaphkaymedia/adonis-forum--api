d9a767cd829e8af7f830ba4b424e1f780963a456
vichekaphkaymedia <vicheka.phkaymedia@gmail.com>
2021-09-19 13:58:14 +0700

    posts data sorting

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,10 +1,14 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post'
+import PostSortingValidator from 'App/Validators/PostSortingValidator';
 import PostValidator from 'App/Validators/PostValidator'
 import UpdatePostValidator from 'App/Validators/UpdatePostValidator';
 
 export default class PostsController {
     public async  index({request}:HttpContextContract) {
+        const validated = await request.validate(PostSortingValidator)
+        let sortBy = validated.sortBy || 'created_at'
+        let orderBy = validated.orderBy || 'desc'
         const page = request.input('page',1)
         const limit = request.input('limit',10)
         const userId = request.input('user_id')
@@ -16,6 +20,7 @@ export default class PostsController {
             if(categoryId,(query) => {
                 query.where('category_id',categoryId)
             })
+            .orderBy(sortBy,orderBy)
             .preload('user')
             .preload('category')
             .preload('comments')
@@ -23,7 +28,7 @@ export default class PostsController {
         return posts;
     }
     public async store({request,auth}:HttpContextContract){
-        const validatedData = await  request.validate(PostValidator)
+        const validatedData = await request.validate(PostValidator)
         const post = await auth.user?.related('posts').create(validatedData)
         await post?.preload('user')
         await post?.preload('category')
