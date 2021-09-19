811f393a33bf8da68021b6a9b582b18d271956e2
vichekaphkaymedia <vicheka.phkaymedia@gmail.com>
2021-09-19 15:12:10 +0700

    Add PostPolicy

---
+++ app/Policies/PostPolicy.ts
@@ -0,0 +1,21 @@
+import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
+import User from 'App/Models/User'
+import Post from 'App/Models/Post'
+
+export default class PostPolicy extends BasePolicy {
+	public async viewList(user: User) {
+        return true
+    }
+	public async view(user: User, post: Post) {
+        return true
+    }
+	public async create(user: User) {
+        return true
+    }
+	public async update(user: User, post: Post) {
+        return user.id === post.user_id
+    }
+	public async delete(user: User, post: Post) {
+        return user.id === post.user_id
+    }
+}
